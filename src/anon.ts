231f0b572df298ec3d540d3dc39c0d54ce741e55
Serhii Varakuta <flood.code@gmail.com>
2024-04-12 12:15:48 +0300

    Do not detach

--- src/anon.ts
+++ src/anon.ts
@@ -1,4 +1,4 @@
-import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { ChildProcess, spawn } from 'child_process';
 import * as os from 'os';
 import { ConfigOptions, createConfigFile } from './config/config';
 import path from 'path';
@@ -8,7 +8,7 @@ import path from 'path';
  */
 export class Anon {
   private options?: ConfigOptions;
-  private process?: ChildProcessWithoutNullStreams;
+  private process?: ChildProcess;
 
   public constructor(options?: ConfigOptions) {
     this.options = options;
@@ -47,46 +47,46 @@ export class Anon {
     this.process = undefined;
   }
 
-  private runBinary(name: string, configPath?: string, onStop?: VoidFunction): ChildProcessWithoutNullStreams {
+  private runBinary(name: string, configPath?: string, onStop?: VoidFunction): ChildProcess {
     const platform = os.platform();
     const arch = os.arch();
-  
+
     let binaryPath = path.join(__dirname, '..', 'bin', platform, arch, name);
     if (platform === 'win32') {
       binaryPath += '.exe';
     }
-  
+
     let args: Array<string> = [];
     if (configPath !== undefined) {
       args = ['-f', configPath]
     }
-  
-    const child = spawn(binaryPath, args);
-  
+
+    const child = spawn(binaryPath, args, { detached: false });
+
     child.stdout.on('data', (data) => {
       if (this.options?.displayLog === true) {
         console.log(`${data}`);
       }
     });
-  
+
     child.stderr.on('data', (data) => {
       if (this.options?.displayLog === true) {
         console.log(`${data}`);
       }
     });
-  
+
     child.on('close', (code) => {
       if (onStop !== undefined) {
         onStop();
       }
     });
-  
+
     child.on('exit', (code) => {
       if (onStop !== undefined) {
         onStop();
       }
     });
-  
+
     return child;
   }
 }
