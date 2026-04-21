e708d569b9222d678ccf05ab1e828d6690985390
web3yurii <web3yurii@gmail.com>
2026-04-21 21:09:50 +0200

    Bump anon version to 0.4.9.13

--- src/postinstall.ts
+++ src/postinstall.ts
@@ -10,7 +10,7 @@ import { getBinaryDir } from './utils';
 
 const owner = 'anyone-protocol';
 const repo = 'ator-protocol'
-const version = 'v0.4.9.11';
+const version = 'v0.4.9.13';
 const releaseUrl = `https://api.github.com/repos/${owner}/${repo}/releases/tags/${version}`;
 
 interface Asset {
@@ -33,27 +33,6 @@ const archMap: { [name: string]: string } = {
   'x64': 'amd64',
 };
 
-// todo - replace web3yurii github url with anyone main branch
-const geoipFiles = [
-  {
-    name: 'geoip',
-    url: 'https://raw.githubusercontent.com/web3yurii/ator-protocol/refs/heads/feature/vpn/src/config/geoip',
-  },
-  {
-    name: 'geoip6',
-    url: 'https://raw.githubusercontent.com/web3yurii/ator-protocol/refs/heads/feature/vpn/src/config/geoip6',
-  },
-];
-
-const downloadTextFile = async (url: string, outputPath: string) => {
-  const response = await axios.get(url, { responseType: 'stream' });
-  const writer = fs.createWriteStream(outputPath);
-  response.data.pipe(writer);
-  return new Promise<void>((resolve, reject) => {
-    writer.on('finish', resolve);
-    writer.on('error', reject);
-  });
-};
 
 const downloadFile = async (url: string, outputPath: string) => {
   try {
@@ -172,13 +151,6 @@ const makeExecutable = (file: string) => {
   for (const file of files) {
     await makeExecutable(path.join(extractDest, file));
     await fs.promises.copyFile(path.join(extractDest, file), path.join(binaryDir, file));
-  }
-
-  // After downloading binary files
-  for (const geo of geoipFiles) {
-    const dest = path.join(binaryDir, geo.name);
-    console.log(`Downloading ${geo.name}...`);
-    await downloadTextFile(geo.url, dest);
   }
 
   console.log('Download complete');
